3bc88562c01cdff35ec55fb8f22f44f4b8583a28
utkuglsvn <utku.glsvn@gmail.com>
2019-01-02 21:35:12 +0300

    Update HanoiTower.java

--- algorithms/src/HanoiTower.java
+++ algorithms/src/HanoiTower.java
@@ -1,8 +1,13 @@
-public class HanoiTower {
+// STOPSHIP: 2.01.2019
+//utkuglsvn
+public class HanoiTower
+{
     static void hanoiTower(int n,String left,String middle,String right) {
-        if(n==1) { System.out.println("Disk:"+n+" "+left + "->" + right);
-        } else {
-            hanoiTower(n - 1, left,right ,middle);
+            if(n==1) {
+                System.out.println("Disk:"+n+" "+left + "->" + right);
+        }
+            else {
+                hanoiTower(n - 1, left,right ,middle);
             System.out.println("Disk:"+n+" "+left + "->" + right);
             hanoiTower(n - 1, middle, left, right);
         }
